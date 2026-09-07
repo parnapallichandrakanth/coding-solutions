078ad37342a5d09f0252f11d96ca17d66353276c
parnapallichandrakanth <parnapallichandrakanth45@gmail.com>
2026-09-07 19:34:46 +0530

    ✅ BIEAT | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-07

--- codechef/medium/BIEAT/solution.py
+++ codechef/medium/BIEAT/solution.py
@@ -1,9 +1,6 @@
 N=int(input())
 A=list(map(int,input().split()))
 M=int(input())
-for i in range(len(A)):
-    A[i]=A[i]//2**M 
 for i in A:
-    print(i,end=" ")
-    
-    
+    i=i//2**M 
+print(A)
