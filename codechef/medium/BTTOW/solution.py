528feb73ccd256c43d7a87abc5cda261a319831b
parnapallichandrakanth <parnapallichandrakanth45@gmail.com>
2026-09-07 19:52:22 +0530

    ✅ BTTOW | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-07

--- codechef/medium/BTTOW/solution.py
+++ codechef/medium/BTTOW/solution.py
@@ -1,1 +1,9 @@
-# cook your dish here
+N,K=map(int,input().split())
+A=list(map(int,input().split()))
+for i in range(len(A)):
+    if A[i]-K>0:
+        A[i]-=K 
+    else:
+        A[i]+=K 
+print(A)
+        
