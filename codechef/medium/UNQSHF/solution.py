711367026435daad3642e68be5e2c1556df9ee24
parnapallichandrakanth <parnapallichandrakanth45@gmail.com>
2026-09-09 20:46:33 +0530

    ✅ UNQSHF | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/UNQSHF/solution.py
+++ codechef/medium/UNQSHF/solution.py
@@ -6,10 +6,10 @@ for _ in range(t):
     freq_A={}
     freq_B={}
     for i in A:
-        freq_A[i]+=freq_A.get(i,0)+1
+        freq_A[i]=freq_A.get(i,0)+1
     for i in B:
-        freq_B[i]+=freq_B.get(i,0)+1
-    if freq_A[a]==freq_B[b] and freq_A[b]==freq[a]:
+        freq_B[i]=freq_B.get(i,0)+1
+    if freq_A['a']==freq_B['b']:
         print("YES")
     else:
         print("NO")
