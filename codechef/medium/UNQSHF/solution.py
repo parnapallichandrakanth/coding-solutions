fbf6f6751bc06e12e6b24acf3165eb074de9141f
parnapallichandrakanth <parnapallichandrakanth45@gmail.com>
2026-09-09 20:53:33 +0530

    ✅ UNQSHF | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/UNQSHF/solution.py
+++ codechef/medium/UNQSHF/solution.py
@@ -3,13 +3,13 @@ for _ in range(t):
     n=int(input())
     A=input()
     B=input()
-    freq_A={'a':0}
-    freq_B={'b':0}
+    freq_A={'a':0,'b':0}
+    freq_B={'a':0,'b':0}
     for i in A:
-        freq_A[i]=freq_A.get(i,0)+1
+        freq_A[i]+=freq_A.get(i,0)+1
     for i in B:
-        freq_B[i]=freq_B.get(i,0)+1
-    if freq_A['a']==freq_B['b'] and freq_B['a']==freq_A['b']:
+        freq_B[i]+=freq_B.get(i,0)+1
+    if freq_B['a']==freq_A['b']:
         print("YES")
     else:
         print("NO")
