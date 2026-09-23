bed45ea74f01d569b4f551655cb07d28322b4e31
parnapallichandrakanth <parnapallichandrakanth45@gmail.com>
2026-09-23 20:53:49 +0530

    ✅ FALLPR | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/FALLPR/solution.py
+++ codechef/medium/FALLPR/solution.py
@@ -4,9 +4,4 @@ for _ in range(t):
     arr=list(map(int,input().split()))
     s=sum(arr)
     m=min(arr)
-    if s>=0:
-        print("Yes")
-    elif s-m>=0:
-        print("Yes")
-    else:
-        print("No")
+    
