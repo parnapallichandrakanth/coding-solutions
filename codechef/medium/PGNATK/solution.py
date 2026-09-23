460aba94313e138f6ddca1e41c3b6a89ec0d0694
parnapallichandrakanth <parnapallichandrakanth45@gmail.com>
2026-09-23 20:44:56 +0530

    ✅ PGNATK | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/PGNATK/solution.py
+++ codechef/medium/PGNATK/solution.py
@@ -2,19 +2,13 @@ t=int(input())
 for _ in range(t):
     n,k=map(int,input().split())
     cnt=0
-    i=0
-    j=2
-    if n<k:
-        print(n)
-    else:
-        while i<n:
-            if i==k:
-                cnt+=1
-                k=k*j 
-                j+=1
-            i+=1
-            cnt+=1
-        print(cnt+1)
+    work=0
+    while work<n:
+        cnt+=1
+        if work%k!=0:
+            work+=1
+    print(cnt)
+        
             
             
             
