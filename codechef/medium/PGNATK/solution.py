18164a276035106cf3ec08886d4acd4e64612ba1
parnapallichandrakanth <parnapallichandrakanth45@gmail.com>
2026-09-23 20:31:42 +0530

    ✅ PGNATK | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/PGNATK/solution.py
+++ codechef/medium/PGNATK/solution.py
@@ -1,3 +1,19 @@
 t=int(input())
 for _ in range(t):
     n,k=map(int,input().split())
+    cnt=0
+    i=0
+    j=2
+    while i<n:
+        if i==k:
+            cnt+=1
+            k=k*j 
+            j+=1
+        i+=1
+        cnt+=1
+    print(cnt)
+            
+            
+            
+        
+        
